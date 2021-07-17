6674290ec9d0992c2ca07979a844c20b6f708622
Isha kundra <kndrsh@gmail.com>
2021-07-16 21:59:01 -0700

    bubble layout

---
+++ app.js
@@ -0,0 +1,49 @@
+// Belly Button Biodiversti - Plotly.js 
+
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata= data.metadata;
+      var resultsarray= metadata.filter(sampleobject => 
+        sampleobject.id == sample);
+      var result= resultsarray[0]
+      var panel = d3.select("#sample-metadata");
+      panel.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        panel.append("h6").text(`${key}: ${value}`);
+      });
+  
+    //buildGauge(result.wfreq)
+  
+  
+  
+    });
+  }
+  
+  //function buildGauge(wfreq) {}
+  
+  function buildCharts(sample) {
+  
+  // Use `d3.json` to fetch the sample data for the plots
+  d3.json("samples.json").then((data) => {
+    var samples= data.samples;
+    var resultsarray= samples.filter(sampleobject => 
+        sampleobject.id == sample);
+    var result= resultsarray[0]
+  
+    var ids = result.otu_ids;
+    var labels = result.otu_labels;
+    var values = result.sample_values;
+  
+  //------------------------------------------------------//
+  //------------------------------------------------------//
+            // Build a BUBBLE Chart 
+  //------------------------------------------------------//
+  //------------------------------------------------------//
+  
+    var LayoutBubble = {
+      margin: { t: 0 },
+      xaxis: { title: "OTU ID" },
+      hovermode: "closest",
+      };
+  
+ 
