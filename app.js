e95851607ec8335611b76706c2c4d1fe756fed7b
Isha kundra <kndrsh@gmail.com>
2021-07-16 22:12:37 -0700

    updated commits

--- app.js
+++ app.js
@@ -46,4 +46,76 @@ function buildMetadata(sample) {
       hovermode: "closest",
       };
   
- 
+      var DataBubble = [ 
+        {
+          x: ids,
+          y: values,
+          text: labels,
+          mode: "markers",
+          marker: {
+            color: ids,
+            size: values,
+            }
+        }
+      ];
+    
+      Plotly.newPlot("bubble", DataBubble, LayoutBubble);
+    
+    
+    //---------------------------------------------------------//
+    //---------------------------------------------------------//
+                  //  Build a BAR Chart
+    //---------------------------------------------------------//  
+    //---------------------------------------------------------// 
+      var bar_data =[
+        {
+          y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+          x:values.slice(0,10).reverse(),
+          text:labels.slice(0,10).reverse(),
+          type:"bar",
+          orientation:"h"
+    
+        }
+      ];
+    
+      var barLayout = {
+        title: "Top 10 Bacteria Cultures Found",
+        margin: { t: 30, l: 150 }
+      };
+    
+      Plotly.newPlot("bar", bar_data, barLayout);
+    });
+    }
+     
+    
+    function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+    
+    // Use the list of sample names to populate the select options
+    d3.json("samples.json").then((data) => {
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+    
+      // Use the first sample from the list to build the initial plots
+      const firstSample = sampleNames[0];
+      buildCharts(firstSample);
+      buildMetadata(firstSample);
+    });
+    }
+    
+    function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
+    }
+    
+    
+    
+    // Initialize the dashboard
+    init();
